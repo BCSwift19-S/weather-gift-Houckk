29dd27f513c84077e11851c71e53ed214a0afb3f
Kenyan Houck <kenyan@macbook.local>
2019-03-06 01:55:14 -0500

    Section 7.2 Complete

---
+++ WeatherGift/ListVC.swift
@@ -0,0 +1,18 @@
+//
+//  ListVC.swift
+//  WeatherGift
+//
+//  Created by Kenyan Houck on 3/6/19.
+//  Copyright © 2019 Kenyan Houck. All rights reserved.
+//
+
+import UIKit
+
+class ListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
---
+++ WeatherGift/PageVC.swift
@@ -0,0 +1,22 @@
+//
+//  PageVC.swift
+//  WeatherGift
+//
+//  Created by Kenyan Houck on 3/6/19.
+//  Copyright © 2019 Kenyan Houck. All rights reserved.
+//
+
+import UIKit
+
+class PageVC: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
