06088298e07a8e7093642addd3edf7148558cd1f
Kenyan Houck <kenyan@macbook.local>
2019-03-16 12:45:41 -0400

    Section 7.10 Complete

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Kenyan Houck on 3/16/19.
+//  Copyright © 2019 Kenyan Houck. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name = ""
+    var coordinates = ""
+}
