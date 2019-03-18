6f2a7c5a0a2c119cb57b034ec67d5a59e56dd395
Kenyan Houck <kenyan@macbook.local>
2019-03-17 23:34:35 -0400

    Section 7.15 Complete

---
+++ WeatherGift/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  WeatherGift
+//
+//  Created by Kenyan Houck on 3/17/19.
+//  Copyright © 2019 Kenyan Houck. All rights reserved.
+//
+
+import Foundation
+
+let urlBase = "https://api.darksky.net/forecast/"
+let urlAPIKey = "eedd395a8c3b2074eccdd1962986d35a/"
